236962a5d1313d06715269f0ea4e90e1fd7598b8
grzechu <grzegorz.jamrooz@gmail.com>
2023-11-25 11:54:39 +0100

    add tests for method setData in ApiRequest

---
+++ tests/Unit/Utility/ApiRequestTest/SetDataTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Utility\ApiRequestTest;
+
+use PHPUnit\Framework\TestCase;
+use Tests\Variant\Utility\ApiRequestVariant;
+
+class SetDataTest extends TestCase
+{
+    public function testShouldSetEmptyArray()
+    {
+        // Given
+        $apiRequest = new ApiRequestVariant();
+
+        // When
+        $apiRequest->setData([]);
+
+        // Then
+        $this->assertEquals([], $apiRequest->getData());
+    }
+
+    public function testShouldSetArrayWithData()
+    {
+        // Given
+        $apiRequest = new ApiRequestVariant();
+        $expected = ['name' => 'Guitar'];
+
+        // When
+        $apiRequest->setData($expected);
+
+        // Then
+        $this->assertEquals($expected, $apiRequest->getData());
+    }
+}
